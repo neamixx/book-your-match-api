0c61693472803ceab6074d30fbb2922ecd6c0cb7
Martinazoo <paumartinnadal@gmail.com>
2025-05-03 04:38:33 +0200

    Responses de la api de skyscanner

--- app/routes/recomanador.py
+++ app/routes/recomanador.py
@@ -19,6 +19,7 @@ CIUTATS = {
 }
 
 # Models per validar entrada
+# Moure aixo al models 
 class PreferenciesUsuari(BaseModel):
     usuari: str
     preferencies: Dict[str, float]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine
 from app import models
-from app.routes import auth, skyscanner
+from app.routes import auth, skyscanner, recomanador
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,3 +9,4 @@ app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(recomanador.router)
+app.include_router(skyscanner.router)
